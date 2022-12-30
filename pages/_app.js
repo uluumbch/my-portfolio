fb575b34f220827b574e20f3f725276da84dc871
Bachrul Uluum <Bachrul Uluum>
2022-12-31 06:58:32 +0800

    edit title page

--- pages/_app.js
+++ pages/_app.js
@@ -3,12 +3,19 @@ import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import { useTheme } from "next-themes";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const { theme, setTheme } = useTheme();
 
   return (
     <div className="dark:bg-body-dark-mode bg-body-primary selection:bg-color-accent">
+       <Head>
+        <title>Bachrul Uluum</title>
+        <meta name="description" content="Bachrul Uluum's Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      
+      </Head>
       <ThemeProvider attribute="class" themes={theme}>
         <Navbar />
         <Component {...pageProps} />
