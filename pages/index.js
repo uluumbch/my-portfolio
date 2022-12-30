d2420bef24e3c2121de07202fbc7e0b6f66a3c9e
Bachrul Uluum <Bachrul Uluum>
2022-12-31 06:55:11 +0800

    add title page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Contact from "../components/Contact";
 import Experience from "../components/Experience";
 import HomePage from "../components/HomePage";
@@ -8,6 +9,12 @@ import SocialLinks from "../components/SocialLinks";
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Bachrul Uluum</title>
+        <meta name="description" content="Bachrul Uluum's Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      
+      </Head>
       <SocialLinks />
       <HomePage />
       <Portfolio />
